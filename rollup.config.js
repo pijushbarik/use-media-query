ea0c27e0608df95f0c90677333ae1407a7ae0f57
Pijush Barik <pijush.barik8@gmail.com>
2021-11-23 23:21:54 +0530

    initial commit

---
+++ rollup.config.js
@@ -0,0 +1,25 @@
+export default {
+  input: "index.js",
+  output: [
+    // ES
+    {
+      file: "es/index.js",
+      format: "esm",
+    },
+    // CommonJS
+    {
+      file: "lib/index.js",
+      format: "cjs",
+    },
+    // UMD
+    {
+      file: "dist/index.js",
+      format: "umd",
+      name: "useMediaQuery",
+      globals: {
+        react: "React",
+      },
+    },
+  ],
+  external: ["react"],
+};
